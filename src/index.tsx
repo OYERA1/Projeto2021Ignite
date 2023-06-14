e189615e4c1857b664f5f324cd368125473ff53e
Oyera <7.chiacherini@gmail.com>
2023-06-14 15:37:36 -0300

    last commit

--- src/index.tsx
+++ src/index.tsx
@@ -5,47 +5,28 @@ import { Model, createServer } from "miragejs";
 import { TransactionTable } from "./components/Transactions";
 
 createServer({
-
   models: {
     transaction: Model,
   },
 
   seeds(server) {
     server.db.loadData({
-      transactions: [
-        {
-          id: 1,
-          title: 'Freelancer de website',
-          type: 'deposit',
-          category: 'Dev',
-          amount: 6000,
-          createdAt: new Date('2022-02-12 09:00:00'),
-        },
-        {
-          id: 2,
-          title: 'Aluguel',
-          type: 'withdraw',
-          category: 'Casa',
-          amount: 1100,
-          createdAt: new Date('2022-02-14 11:00:00'),
-        }
-      ]
-    })
+      transactions: [],
+    });
   },
 
   routes() {
     this.namespace = "api";
 
     this.get("transactions", () => {
-      return this.schema.all('transaction')
+      return this.schema.all("transaction");
     });
 
     this.post("transactions", (schema, req) => {
-      const data = JSON.parse(req.requestBody)
+      const data = JSON.parse(req.requestBody);
 
-      return schema.create('transaction', data)
-
-    })
+      return schema.create("transaction", data);
+    });
   },
 });
 
